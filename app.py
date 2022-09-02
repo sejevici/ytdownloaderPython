7942b7a13a2dda988c739f24d018df9530d63de8
sejevici <sejevici@gmail.com>
2022-09-02 15:12:16 +0000

    yea yea

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
+from typing import List
+from uuid import UUID, uuid4
 from fastapi import FastAPI
+
+from models import Gender, Role, User
 
 app = FastAPI()
 
-inventory = {
-    1: {
-        "Name": "Milk",
-        "price": 3.98,
-        "Brand": "Arla"
-    }
+db: List[User] = [
+    User(
+        id=UUID("3963001e-233d-40d1-9754-74667a631b70"),
+        first_name="Victor",
+        last_name="Bischoff",
+        gender=Gender.male,
+        roles=[Role.admin]
+    ),
+    User(
+        id=UUID("cff7f082-dfe6-4f8c-83e6-cc8fd3da787d"),
+        first_name="viggo",
+        last_name="Bischoff",
+        gender=Gender.male,
+        roles=[Role.student]
+    )
+]
+@app.get("/")
+async def root():
+    return {"Hello": "WORLD"}
 
-}
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db;
 
-@app.get("/get_item/{item_id}")
-def get_item(item_id: int):
-    return inventory[item_id]
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
+
